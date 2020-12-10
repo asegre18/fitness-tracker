88e895baa1d199eb1a541c0789a4e4611497bc5c
Andrew Segre <asegre18@cmc.edu>
2020-12-10 11:28:34 -0800

    deploying

--- index.js
+++ index.js
@@ -16,8 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", 
+  {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
