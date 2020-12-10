2566b90aa23f29f9f63400ce12c149079978427d
Andrew Segre <asegre18@cmc.edu>
2020-12-10 11:01:40 -0800

    fixed file structure

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv')
+  .config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -14,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
