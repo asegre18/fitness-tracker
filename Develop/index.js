19885a78635452e7229b567b8a99f4d53064a86a
Andrew Segre <asegre18@cmc.edu>
2020-12-09 21:21:32 -0800

    init repo

---
+++ Develop/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
